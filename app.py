2843042eab5144697abe95a64b06a105164ef23e
Jie-Green <ryokujie48@gmail.com>
2023-10-07 14:21:42 +0900

    first commit

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])
-def index():
-    if request.method == "GET":
-        return render_template('index.html')
-    else:
-        message = request.form['message']
-        return render_template("index.html", message=message)
+# @app.route("/", methods=["GET"])
+@app.get("/")
+def index_get():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
- 
+
+# @app.route("/", methods=["POST"])
+@app.post("/")
+def index_post():
+    message = request.form["message"]
+    return render_template("index.html", message=message)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
